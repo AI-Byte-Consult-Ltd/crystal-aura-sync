7e2ae4aa71d6366dcf9f693b2d23c1e5c091aa61
Alexander Lunin <info@aibyteconsult.com>
2025-10-17 19:55:54 +0000

    deploy: update build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // 👈 ОБЯЗАТЕЛЬНО для GitHub Pages / custom domain
   server: {
     host: "::",
     port: 8080,
